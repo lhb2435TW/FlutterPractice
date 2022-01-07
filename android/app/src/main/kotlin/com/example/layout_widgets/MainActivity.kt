e5cd456761dc53c8dae498aebeb47e34cf890c81
lhb2435 <lhb2435@twinny.co.kr>
2022-01-07 18:11:57 +0900

    플랫폼 채널 예제 작성

--- android/app/src/main/kotlin/com/example/layout_widgets/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/layout_widgets/MainActivity.kt
@@ -1,6 +1,36 @@
 package com.example.layout_widgets
 
+import android.os.BatteryManager
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+import android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import androidx.annotation.RequiresApi
 
 class MainActivity: FlutterActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor, CHANNEL_BATTERY)
+            .setMethodCallHandler { call, result ->
+                if (METHOD_BATTERY == call.method) {
+                    val manager = getSystemService(BATTERY_SERVICE) as BatteryManager
+                    val battery = manager.getIntProperty(BATTERY_PROPERTY_CAPACITY)
+                    result.success(battery)
+                }
+                result.notImplemented()
+            }
+    }
+
+    companion object {
+        const val METHOD_BATTERY = "getBatteryLevel";
+        const val CHANNEL_BATTERY = "android/battery";
+    }
 }
---
+++ lib/channel/battery_channel_demo_v2.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(BatteryPage());
+
+class BatteryPage extends StatefulWidget {
+  @override
+  State createState() => BatteryPageState();
+}
+
+class BatteryPageState extends State<BatteryPage> {
+  String _text = '배터리 잔량: 모름';
+
+  static const String CHANNEL_BATTERY = 'android/battery';
+  static const String METHOD_BATTERY = 'getBatteryLevel';
+  static const MethodChannel batteryChannel = MethodChannel(CHANNEL_BATTERY);
+
+  _refresh() async {
+    print('refresh battery level');
+
+    String _newText;
+    try {
+      final int result = await batteryChannel.invokeMethod(METHOD_BATTERY);
+      _newText = '배터리 잔량: $result %';
+    } on PlatformException {
+      _newText = '배터리 잔량을 알 수 없습니다.';
+    }
+
+    setState(() {
+      _text = _newText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '배터리 채널 데모 V2',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('배터리 채널 데모 V2')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_text),
+              RaisedButton(
+                child: Text('가져오기'),
+                onPressed: _refresh,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
