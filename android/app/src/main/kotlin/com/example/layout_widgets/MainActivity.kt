6e987bff60aafe8bf3762204a206a59041b33eb6
lhb2435 <lhb2435@twinny.co.kr>
2022-01-10 12:59:28 +0900

    플랫폼 채널을 통해 위치 정보 가져오기 예제 작성

--- android/app/src/main/kotlin/com/example/layout_widgets/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/layout_widgets/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.layout_widgets
 
+import android.annotation.SuppressLint
 import android.os.BatteryManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -10,13 +11,28 @@ import android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.annotation.RequiresApi
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity: FlutterActivity() {
+
+    lateinit var fusedLocationClient: FusedLocationProviderClient
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        MethodChannel(flutterEngine.dartExecutor, CHANNEL_LOCATION)
+            .setMethodCallHandler { call, result ->
+                Log.d(TAG, "location_channel :: method ${call.method}")
+                if (METHOD_CURRENT_LOCATION == call.method) {
+                    getCurrentLocation(result)
+                }
+            }
 
         MethodChannel(flutterEngine.dartExecutor, CHANNEL_BATTERY)
             .setMethodCallHandler { call, result ->
@@ -29,8 +45,21 @@ class MainActivity: FlutterActivity() {
             }
     }
 
+    @SuppressLint("MissingPermission")
+    private fun getCurrentLocation(result: MethodChannel.Result) {
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener(this) { location ->
+                val res = "(${location.latitude}, ${location.longitude})"
+                Log.d(TAG, "location? $res")
+                result.success(res)
+            }
+    }
+
     companion object {
+        const val TAG = "PlatformChannels"
         const val METHOD_BATTERY = "getBatteryLevel";
         const val CHANNEL_BATTERY = "android/battery";
+        const val METHOD_CURRENT_LOCATION = "getCurrentLocation"
+        const val CHANNEL_LOCATION = "android/location"
     }
 }
---
+++ lib/location/location_channel_demo_v2.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main() => runApp(LocationPage());
+
+class LocationPage extends StatefulWidget {
+  @override
+  State createState() => LocationPageState();
+}
+
+class LocationPageState extends State<LocationPage> {
+  String _text = '현재 위치: 모름';
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPermission();
+  }
+
+  _checkPermission() async {
+    if (await Permission.location.request().isGranted) {
+      print('권한 수락');
+    } else {
+      print('권한 거부됨');
+    }
+  }
+
+  _refresh() async {
+    print('refresh current location');
+
+    String _newText;
+    try {
+      Position position = await Geolocator().getCurrentPosition(desiredAccuracy:
+      LocationAccuracy.best);
+      String result = "(${position.latitude}, ${position.longitude})";
+      _newText = '현재 위치는 $result';
+    } on PlatformException {
+      _newText = '현재 위치를 사용할 수 없습니다.';
+    }
+
+    setState(() {
+      _text = _newText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '현재 위치 채널 데모 V2',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('현재 위치 채널 데모 V2')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_text),
+              RaisedButton(
+                child: Text('가져오기'),
+                onPressed: _refresh,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
